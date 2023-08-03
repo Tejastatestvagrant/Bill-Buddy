a43543d5e0e8914f24e936e61ee190f02a004e47
Rajneesh-S <140791190+Rajneesh-S@users.noreply.github.com>
2023-08-03 15:49:08 +0530

    added basic functions without private members-rajneesh

--- User.js
+++ User.js
@@ -1,32 +1,35 @@
 export default class User{
-    #name;
     #password;
-    #expenses = [];
-    #owes = [];
-    #group = [];
     constructor(name, password){
-        this.#name = name;
+        this.name = name;
         this.#password = password;
+        this.expenses = [];
+        this.groups= [];
+        this.owes= [];
     }
+
+
 
     authentication(username,password)
     {
-        return (this.name===username && this.password===password);
+        return (this.name===username && this.#password===password);
     }
 
     addexpense(expense)
     {
-        this.#expenses.push(expense);
+        this.expenses.push(expense);
     }
 
     addgroup(group)
     {
-        this.#group.push(group);
+        this.group.push(group);
     }
     addowes(owes)
     {
-        this.#owes.push(owes);
+        this.owes.push(owes);
     }
+
     
+
 
 }
