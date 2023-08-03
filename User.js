05758ebe92fd9bbebf4f9b22615a3fc72638842d
Amrutha <amruthar@testvagrant.com>
2023-08-03 15:29:02 +0530

    added constructor in user

--- User.js
+++ User.js
@@ -1,3 +1,12 @@
 export default class User{
-    
+    #name;
+    #password;
+    #expenses = [];
+    #owes = [];
+    #group = [];
+    constructor(name, password){
+        this.#name = name;
+        this.#password = password;
+    }
+
 }
