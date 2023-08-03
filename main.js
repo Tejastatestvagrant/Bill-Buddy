42904c0cea266affb2f50b2945d3ea77dc4e6fcd
Tejastatestvagrant <139124793+Tejastatestvagrant@users.noreply.github.com>
2023-08-03 15:01:44 +0530

    phase-1 version-1

---
+++ main.js
@@ -0,0 +1,31 @@
+function validRegistor(){
+    let username=document.getElementById('username').value;
+    let pwd=document.getElementById('pwd').value;
+    let cpwd=document.getElementById('cpwd').value;
+
+    let errorname='';
+    const regex = /^[a-zA-Z0-9_-]{3,20}$/;
+    if (!regex.test(username)) {
+        errorname.concat('invalid username');
+        
+      }
+
+      const pwdregex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d\W]{8,}$/;
+      if (pwdregex.test(pwd)) 
+       {
+         if(pwd!==cpwd)
+         {
+            errorname.concat("entered ummatch pwd ")
+            
+         }
+       
+      else
+      {
+         
+        errorname.concat("invalid pwd")
+        
+      }
+      document.getElementById('nameError').innerHTML(`${errorname}`)
+    
+}
+}
