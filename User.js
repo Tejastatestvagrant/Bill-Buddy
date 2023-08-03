9b09283d638d13ec4849831f0cef2f72f94af7d4
Rajneesh-S <140791190+Rajneesh-S@users.noreply.github.com>
2023-08-03 15:39:16 +0530

    added basic methods-Rajneesh

--- User.js
+++ User.js
@@ -9,4 +9,24 @@ export default class User{
         this.#password = password;
     }
 
+    authentication(username,password)
+    {
+        return (this.name===username && this.password===password);
+    }
+
+    addexpense(expense)
+    {
+        this.#expenses.push(expense);
+    }
+
+    addgroup(group)
+    {
+        this.#group.push(group);
+    }
+    addowes(owes)
+    {
+        this.#owes.push(owes);
+    }
+    
+
 }
